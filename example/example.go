7ed0a89909491241004bb028a3d399da448df439
Lawrence E. Bakst <leb@me.com>
2019-08-13 02:48:09 -0400

    improved code readability and speed fixes, now only 2X slower than chans

--- example/example.go
+++ example/example.go
@@ -1,10 +1,14 @@
-// Copyright © 2016,2017 Lawrence E. Bakst. All rights reserved.
+// Copyright © 2016,2017, 2019 Lawrence E. Bakst. All rights reserved.
+
+// Send ints from a producer to a consumer and verify monotonicity.
+// Uses ring buffer or channel.
 
 package main
 
 import (
 	"flag"
 	"fmt"
+	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -16,7 +20,7 @@ import (
 )
 
 var n = flag.Int("n", 10*1000*1000, "n")
-var s = flag.Int("s", 10, "ring size")
+var s = flag.Int("s", 10, "ring size power of 2 exponent")
 var iles = flag.Int("iles", 10, "how many buckets for histogram")
 var cf = flag.Bool("cf", false, "chans not concurrent ring")
 var lf = flag.Bool("lf", false, "benchmark loads per second")
@@ -33,6 +37,7 @@ func tdiff(begin, end time.Time) time.Duration {
 }
 
 func producer(r *ring.Ring, n int) {
+	runtime.LockOSThread()
 	for i := 0; i < n; i++ {
 	retry:
 		if r.Put(i) {
@@ -47,6 +52,7 @@ func producer(r *ring.Ring, n int) {
 
 func consumer(r *ring.Ring, n int) {
 	var cnt int
+	runtime.LockOSThread()
 	for i := 0; i < n; i++ {
 	retry:
 		v, b := r.Get()
@@ -93,7 +99,7 @@ func chanConsumer(c chan int, n int) {
 
 var ui uint64 = 3
 
-// loads per second
+// 64 bit loads per second
 func lps(n int) uint64 {
 	var totui uint64
 
@@ -113,7 +119,7 @@ func lps(n int) uint64 {
 	return totui
 }
 
-// loads per second
+// atomic 64 bit loads per second
 func alps(n int) uint64 {
 	var totui uint64
 
@@ -131,6 +137,19 @@ func alps(n int) uint64 {
 		totui += atomic.LoadUint64(p)
 	}
 	return totui
+}
+
+func basiTest() {
+	r := ring.New(*s)
+	fmt.Printf("size=%d\n", unsafe.Sizeof(r))
+	r.Put(1)
+	r.Put(2)
+	r.Put(3)
+	r.Put(4)
+	for i := 0; i <= 4; i++ {
+		v, b := r.Get()
+		fmt.Printf("b=%v, v=%v\n", b, v)
+	}
 }
 
 func main() {
@@ -165,27 +184,13 @@ func main() {
 	opsSec := float64(*n) / dur.Seconds() * 1000
 	fmt.Printf("%0.2h\n", hrff.Float64{opsSec, "ops/sec"})
 	if !*cf {
-		fmt.Printf("Ops=%.1h, Put Spins=%.1h, Get Spins=%.1h, ss=%.1h, sf=%.1h\n",
+		fmt.Printf("Ops=%.1h, Put Spins=%.1h, Get Spins=%.1h, Puts Full=%h, Gets Empty=%h\n",
 			hrff.Float64{V: float64(*n), U: "ops"},
-			hrff.Float64{V: float64(r.Ps), U: "spins"}, hrff.Float64{V: float64(r.Gs), U: "spins"},
-			hrff.Float64{V: float64(r.Ss), U: "stores"}, hrff.Float64{V: float64(r.Sf), U: "stores"})
-		/*
-			ps := stats.New(pstats, int64(*iles))
-			cs := stats.New(cstats, int64(*iles))
-			fmt.Printf("ps=%v\n", ps)
-			fmt.Printf("cs=%v\n", cs)
-		*/
+			hrff.Float64{V: float64(r.PutSpins), U: "spins"}, hrff.Float64{V: float64(r.GetSpins), U: "spins"},
+			hrff.Int64{V: r.PutsFull}, hrff.Int64{V: r.GetsEmpty})
+		fmt.Printf("SwapsSucceeded=%.1h, SwapsFailed=%.1h\n",
+			hrff.Float64{V: float64(r.SwapsSucceeded), U: "stores"}, hrff.Float64{V: float64(r.SwapsFailed), U: "stores"})
 	}
-	//time.Sleep(10 * time.Second)
 	return
 
-	fmt.Printf("size=%d\n", unsafe.Sizeof(r))
-	r.Put(1)
-	r.Put(2)
-	r.Put(3)
-	r.Put(4)
-	for i := 0; i <= 4; i++ {
-		v, b := r.Get()
-		fmt.Printf("b=%v, v=%v\n", b, v)
-	}
 }
--- ring.go
+++ ring.go
@@ -1,146 +1,123 @@
-// Copyright © 2016,2017 Lawrence E. Bakst. All rights reserved.
+// Copyright © 2016,2017,2019 Lawrence E. Bakst. All rights reserved.
 
-// Concurrent ring buffer, safe to call from goroutines
-// Currently slower than using a channel by about 3X
+// Experimental, investigation into a concurrent ring buffer, safe to call from goroutines.
+// Uses overflow trick that I did not know about 10 years ago.
+// Currently slower than using a channel by about 3X because of atomic instructions.
+// Now only 2X slower.
 
 package ring
 
 import (
 	_ "fmt"
+	"runtime"
 	"sync/atomic"
 	"unsafe"
 )
 
-type Stats struct {
-	Ps   uint64
-	Gs   uint64
-	Ss   uint64
-	Sf   uint64
-	Gcnt uint64
-	Pcnt uint64
+// State hold the
+type State struct {
+	pidx uint16
+	gidx uint16
+	pad  int32
 }
 
-var Gstats Stats
+// Stats only valid with one producer and consumer
+type Stats struct {
+	Puts           int64
+	Gets           int64
+	PutsFull       int64
+	GetsEmpty      int64
+	SwapsSucceeded int64
+	SwapsFailed    int64
+	PutSpins       int64
+	GetSpins       int64
+}
 
-type Pointer struct {
-	pidx  uint16
-	cidx  uint16
+type Config struct {
 	mask  uint16
 	shift uint8
 	size  uint8
-	//fill  uint8
 }
 
 type Ring struct {
-	Pointer
+	State
+	Config
 	Stats
 	Values []int
 }
 
 func New(size int) (r *Ring) {
-	r = &Ring{Pointer: Pointer{mask: uint16(1<<uint(size)) - 1, shift: uint8(size), size: uint8(size + 1)}}
+	r = &Ring{Config: Config{mask: uint16(1<<uint(size)) - 1, shift: uint8(size), size: uint8(size + 1)}}
 	r.Values = make([]int, 1<<uint(size))
-	//fmt.Printf("r=%v\n", r)
 	return
 }
 
-/*
-func (p *Pointer) load() Pointer {
-	uip := (*uint64)(unsafe.Pointer(p))
-	ui := atomic.LoadUint64(uip)
-	return *(*Pointer)(unsafe.Pointer(&ui))
-}
-
-func cp(dp *uint64, sp *uint64) {
-	*dp = *sp
-}
-
-func store(r *Ring, ov, nv *Pointer) bool {
-	rp := (*uint64)(unsafe.Pointer(r))
-	ovp := (*uint64)(unsafe.Pointer(ov))
-	nvp := (*uint64)(unsafe.Pointer(nv))
-	b := atomic.CompareAndSwapUint64(rp, *ovp, *nvp)
-	if b {
-		atomic.AddUint64(&Gstats.Ss, 1)
-	} else {
-		atomic.AddUint64(&Gstats.Sf, 1)
-	}
-	return b
-}
-*/
-
-func (r *Ring) Put(v int) bool {
+func (r *Ring) Put(value int) (full bool) {
 	var ui uint64
-	var re, ore Pointer
+	var re, ore State
 
-	r.Pcnt = 0
 	for {
-		//re := *(*cring)(unsafe.Pointer(uintptr(atomic.LoadUint64((*uint64)(unsafe.Pointer(r)))))) // load(r)
-		//re := load(r)
-		ui = atomic.LoadUint64((*uint64)(unsafe.Pointer(&r.Pointer)))
-		re = *(*Pointer)(unsafe.Pointer(&ui))
-		//re = (cring)((uintptr)(atomic.LoadUint64((*uint64)(unsafe.Pointer(r)))))
+		ui = atomic.LoadUint64((*uint64)(unsafe.Pointer(&r.State)))
+		re = *(*State)(unsafe.Pointer(&ui))
 		ore = re
 		switch {
-		case re.pidx&re.mask == re.cidx&re.mask && re.pidx>>re.shift != re.cidx>>re.shift:
-			// full
-			return true
-		case re.pidx&re.mask == re.cidx&re.mask && re.pidx>>re.shift == re.cidx>>re.shift:
-			// empty
-			fallthrough
+		case re.pidx&r.mask == re.gidx&r.mask && re.pidx>>r.shift != re.gidx>>r.shift:
+			full = true
+			runtime.Gosched()
+			r.PutsFull++
+			return
+		case re.pidx&r.mask == re.gidx&r.mask && re.pidx>>r.shift == re.gidx>>r.shift:
+			fallthrough // empty
 		default:
-			//fmt.Printf("pidx=%d ", re.pidx)
-			r.Values[re.pidx&re.mask] = v
+			r.Values[re.pidx&r.mask] = value
 			re.pidx++
-			if re.pidx > uint16(1<<re.size)-1 {
+			if re.pidx > uint16(1<<r.size)-1 {
 				re.pidx = 0
 			}
 			swapped := atomic.CompareAndSwapUint64((*uint64)(unsafe.Pointer(r)), *(*uint64)((unsafe.Pointer)(&ore)), *(*uint64)((unsafe.Pointer)(&re))) //store(r, &ore, &re)
 			if swapped {
-				atomic.AddUint64(&r.Ss, 1)
-				return false
+				//atomic.AddInt64(&r.SwapsSucceeded, 1)
+				r.Puts++
+				return
 			}
-			atomic.AddUint64(&r.Sf, 1)
+			//atomic.AddInt64(&r.SwapsFailed, 1)
 		}
-		r.Ps++
-		r.Pcnt++
-		//fmt.Printf("PC ")
+		r.PutSpins++
 	}
 }
 
-func (r *Ring) Get() (int, bool) {
+// Get returns an index
+func (r *Ring) Get() (value int, empty bool) {
 	var ui uint64
-	var re, ore Pointer
+	var re, ore State
 
-	r.Gcnt = 0
 	for {
-		//re := *(*cring)(unsafe.Pointer(uintptr(atomic.LoadUint64((*uint64)(unsafe.Pointer(r)))))) // load(r)
-		ui = atomic.LoadUint64((*uint64)(unsafe.Pointer(r)))
-		re = *(*Pointer)(unsafe.Pointer(&ui))
+		ui = atomic.LoadUint64((*uint64)(unsafe.Pointer(&r.State))) // atomically read the pointer
+		re = *(*State)(unsafe.Pointer(&ui))                         // make two copies
 		ore = re
 		switch {
-		case re.pidx&re.mask == re.cidx&re.mask && re.pidx>>re.shift == re.cidx>>re.shift:
-			// empty
-			return 0, true
-		case re.pidx&re.mask == re.cidx&re.mask && re.pidx>>re.shift != re.cidx>>re.shift:
-			// full
-			fallthrough
+		case re.pidx&r.mask == re.gidx&r.mask && re.pidx>>r.shift == re.gidx>>r.shift:
+			empty = true
+			runtime.Gosched()
+			r.GetsEmpty++
+			return
+		case re.pidx&r.mask == re.gidx&r.mask && re.pidx>>r.shift != re.gidx>>r.shift:
+			fallthrough // full
 		default:
-			//fmt.Printf("cidx=%d ", re.cidx)
-			ret := r.Values[re.cidx&re.mask]
-			re.cidx++
-			if re.cidx > uint16(1<<re.size)-1 {
-				re.cidx = 0
+			v := r.Values[re.gidx&r.mask]
+			re.gidx++
+			if re.gidx > uint16(1<<r.size)-1 {
+				re.gidx = 0
 			}
 			swapped := atomic.CompareAndSwapUint64((*uint64)(unsafe.Pointer(r)), *(*uint64)((unsafe.Pointer)(&ore)), *(*uint64)((unsafe.Pointer)(&re))) //store(r, &ore, &re)
 			if swapped {
-				atomic.AddUint64(&r.Ss, 1)
-				return ret, false
+				//atomic.AddInt64(&r.SwapsSucceeded, 1)
+				value = v
+				r.Gets++
+				return
 			}
-			atomic.AddUint64(&r.Sf, 1)
+			//atomic.AddInt64(&r.SwapsFailed, 1)
 		}
-		r.Gs++
-		r.Gcnt++
-		//fmt.Printf("GC ")
+		r.GetSpins++
 	}
 }
